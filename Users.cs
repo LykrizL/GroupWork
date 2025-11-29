74a7fba293efef905545477ae15c784385ac42c6
Boris Balakin <borisbalakin5@gmail.com>
2025-11-29 09:34:15 +0500

    (Edited repository by BrsBlkn) (Ввод комментариев - подписей к функциям в коде)

--- Users.cs
+++ Users.cs
@@ -3,9 +3,10 @@ using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace WorkLibrary
+// Новые внесенные изменения в код
+namespace WorkLibrary // Название пространства
 {
-    public class Users : People
+    public class Users : People // Название класса
     {
         private string _userLogin;
         private string _userPassword;
@@ -36,12 +37,12 @@ namespace WorkLibrary
                 _userPassword = value;
             }
         }
-        public bool IsValidPassword(string password)
+        public bool IsValidPassword(string password) // Функция проверки пароля
         {
             string passwordPattern = @"^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
             return Regex.IsMatch(password, passwordPattern);
         }
-        public bool IsPasswordСonfirm(string password, string confirmPassword)
+        public bool IsPasswordСonfirm(string password, string confirmPassword) // Функция принятия пароля
         {
             return password == confirmPassword;
         }
@@ -50,7 +51,7 @@ namespace WorkLibrary
             string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
             return Regex.IsMatch(email, emailPattern);
         }
-        public bool IsValidPhone(string phone)
+        public bool IsValidPhone(string phone) // Функция проверки номера телефона
         {
             string phonePattern = @"^\+7\d{10}$";
             return Regex.IsMatch(phone, phonePattern);
