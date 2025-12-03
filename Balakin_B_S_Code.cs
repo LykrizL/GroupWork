d3e954697e542a1b577f32ebe62210076133e12a
Boris Balakin <borisbalakin5@gmail.com>
2025-12-03 11:10:30 +0500

    Балакин: Добавление нового файла.

---
+++ Balakin_B_S_Code.cs
@@ -0,0 +1,6 @@
+﻿// Балакин Новая Функция
+        public bool IsValidPassword(string password) // Функция проверки пароля
+        {
+            string passwordPattern = @"^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+            return Regex.IsMatch(password, passwordPattern);
+        }
